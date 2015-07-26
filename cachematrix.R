3d788a1c22ece035c42c1d259dce95018505b408
victora0007 <victor.saenz.bonilla@gmail.com>
2015-07-25 19:26:15 -0500

    Commit my completed R file into my git repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly. The following functions
+## work together to create a square invertible matrix and make the inverse of the matrix
+## available in the cache environment.
 
-## Write a short comment describing this function
-##
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         cmi <- NULL
-        
         set <- function(y) {
                 x <<- y
                 cmi <<- NULL
         }
-        
         get <- function() x
-        
         setMatrixInv <- function(cminv) cmi <<- cminv
-        
         getMatrixInv <- function() cmi
-        
-        list(set = set, get = get,
-             setMatrixInv = setMatrixInv,
-             getMatrixInv = getMatrixInv)
+        list(set = set, get = get, setMatrixInv = setMatrixInv, getMatrixInv = getMatrixInv)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        cmi <- x$getMatrixInv
-        
-        if (!is.null(cmi)) {
-                message("Getting cached data")
+        cmi <- x$getMatrixInv()
+        if(!is.null(cmi)) {
+                message("Getting cached data.")
                 return(cmi)
         }
-        
         data <- x$get()
-        cmi <- solve(data, ...)
+        cmi <- solve(data)
         x$setMatrixInv(cmi)
         cmi
 }
